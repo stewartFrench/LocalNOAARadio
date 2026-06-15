59f4b949e8ba69d4ac652b931c0297c532be9211
Stewart French <stewart.french@gmail.com>
2026-06-15 14:32:45 -0500

    Initial Commit

---
+++ LocalNOAARadio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalNOAARadio
+//
+//  Created by Stewart French on 6/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalNOAARadio/LocalNOAARadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNOAARadioApp.swift
+//  LocalNOAARadio
+//
+//  Created by Stewart French on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct LocalNOAARadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
